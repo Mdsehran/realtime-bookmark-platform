cf2bdd19b6165310644a921aa09bb483d4db59d9
Mdsehran <113717589+Mdsehran@users.noreply.github.com>
2026-05-07 23:05:54 +0530

    Add image remote patterns to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  typescript: {
-    ignoreBuildErrors: true,
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'www.google.com' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+    ],
   },
 }
 
