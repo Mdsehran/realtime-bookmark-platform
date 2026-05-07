8771157b02e7fcfcc717b8a68bbeaa432a0d8162
Mdsehran <113717589+Mdsehran@users.noreply.github.com>
2026-05-07 22:35:08 +0530

    Refactor cookie handling in auth callback

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 import { NextRequest, NextResponse } from 'next/server'
@@ -9,19 +10,16 @@ export async function GET(request: NextRequest) {
 
   if (code) {
     const cookieStore = await cookies()
+
     const supabase = createServerClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
       {
         cookies: {
-          getAll() { return cookieStore.getAll() },
-         setAll(
-  cookiesToSet: {
-    name: string
-    value: string
-    options?: any
-  }[]
-) {
+          getAll() {
+            return cookieStore.getAll()
+          },
+          setAll(cookiesToSet: { name: string; value: string; options?: any }[]) {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options)
             )
